02173a20c7a4f12bad54e0bcc6bb39eef148ade2
Bedatop <98839707+Bedatop@users.noreply.github.com>
2023-05-26 17:46:15 +0500

    7

---
+++ script.js
@@ -0,0 +1,21 @@
+//let array = [10, 20, 15, 4, 30];
+
+/* for (let i = 0; i <= 3; i++) {
+    array.push(i)
+}
+console.log(array); */
+//for(let i=0; i<array.length; i++) {
+    //console.log(array[i])
+//}
+//for (let key of array) {
+    //console.log(key)
+//}
+let names= 'Иван Петров Степанович';
+//let arr= names.split(' ');
+//console.log(arr)
+//let [name, lastName]=['Иван', 'Петров'];
+//let [name, lastName]=arr;
+let [, lastName, srname]=names.split(' ');
+console.log();
+console.log(lastName);
+console.log(srname)
